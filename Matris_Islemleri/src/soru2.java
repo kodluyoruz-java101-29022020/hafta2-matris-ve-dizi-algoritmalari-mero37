c35b5aa98d791734ab448b1342a2abd9c926ae7e
Mert Demirkıran <56468869+mero37@users.noreply.github.com>
2020-03-17 22:56:40 +0300

    MertDemirkiran

--- Matris_Islemleri/src/soru2.java
+++ Matris_Islemleri/src/soru2.java
@@ -3,11 +3,11 @@ public class soru2 {
 
 	public static void main(String[] args) {
 		int[][] matris = new int[][]{
-            {1,2,3,4,5},
-            {6,7,8,9,10},
-            {11,12,13,14,15},
-            {16,17,18,19,20},
-            {21,22,23,24,25}
+		    {1,2,3,4,5},
+		    {6,7,8,9,10},
+		    {11,12,13,14,15},
+		    {16,17,18,19,20},
+		    {21,22,23,24,25}
 		};;
 		int k=0;
 		int[] dizi = new int[matris.length*matris[0].length];
