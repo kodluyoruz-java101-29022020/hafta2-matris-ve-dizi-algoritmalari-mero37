c34345c3ff515c73c08cd9428f9e5105c1d46755
Mert Demirkıran <56468869+mero37@users.noreply.github.com>
2020-03-17 22:55:47 +0300

    MertDemirkiran

--- Matris_Islemleri/src/soru3.java
+++ Matris_Islemleri/src/soru3.java
@@ -31,8 +31,7 @@ public class soru3 {
 		for(int i=0;i<matris.length;i++) {
 			for(int j=0;j<matris[0].length;j++) {
 				System.out.print(matris[i][j]+" ");
-				
-			}
+		}
 			System.out.println();
 		}
 		System.out.println("--------------------------------------");
